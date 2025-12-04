b6e80020134a8b787491905a00220a35a746a259
Andrew Bruce <andrewbruce.himni@gmail.com>
2025-12-04 03:00:08 -0500

    * renamed `parse_` functions

--- R/parse.R
+++ R/parse.R
@@ -7,14 +7,13 @@
 #' @name parse
 #' @returns character vector of names associated with codes.
 #' @examples
-#' parse_medicare_provider("670055")
-#' parse_medicare_opo("05P001")
-#' parse_emergency_hospital("12345E")
-#' parse_medicare_supplier("10C0001062")
-#' parse_medicaid_only_hospital("A5J508")
-#' parse_medicaid_only_facility("23E301")
-#' parse_eipps_provider("21T101")
-#' parse_eipps_unit("02TA01")
+#' parse_medicare("670055")
+#' parse_opo("05P001")
+#' parse_emergency("12345E")
+#' parse_supplier("10C0001062")
+#' parse_medicaid("A5J508")
+#' parse_unit("21T101")
+#' parse_subunit("02TA01")
 NULL
 
 #' @noRd
@@ -41,7 +40,7 @@ parser <- function(entity,
 # Medicare Provider: 670055 -> 67 0055
 #' @rdname parse
 #' @export
-parse_medicare_provider <- parser(
+parse_medicare <- parser(
   entity = "Medicare Provider",
   regex  = "([0-9]{4})",
   groups = "\\2",
@@ -50,14 +49,14 @@ parse_medicare_provider <- parser(
 # Medicare OPO Provider: 05P001 -> 05 P 001
 #' @rdname parse
 #' @export
-parse_medicare_opo <- parser(
+parse_opo <- parser(
   entity = "Medicare Provider",
   regex  = "([P])([0-9]{3})")
 
 # Emergency Hospital: 12345E -> 12 E 345
 #' @rdname parse
 #' @export
-parse_emergency_hospital <- parser(
+parse_emergency <- parser(
   entity = "Emergency Hospital",
   regex  = "([0-9]{3})([E])",
   groups = "\\3 \\2")
@@ -65,38 +64,31 @@ parse_emergency_hospital <- parser(
 # Medicare Supplier: 10C0001062 -> 10 C 0001062
 #' @rdname parse
 #' @export
-parse_medicare_supplier <- parser(
+parse_supplier <- parser(
   entity = "Medicare Supplier",
   regex  = "([CDX])([0-9]{7})",
   groups = "\\2 \\3")
 
-# Medicaid-Only Hospital: 01L008 -> 01 L 008
-#' @rdname parse
-#' @export
-parse_medicaid_only_hospital <- parser(
-  entity = "Medicaid-Only Provider",
-  regex  = "([J])([0-9]{3})")
-
 # Medicaid-Only Facility: 01L008 -> 01 L 008
 #' @rdname parse
 #' @export
-parse_medicaid_only_facility <- parser(
+parse_medicaid <- parser(
   entity = "Medicaid-Only Provider",
-  regex  = "([ABE-HKL])([0-9]{3})")
+  regex  = "([ABE-HJ-L])([0-9]{3})")
 
 # IPPS-Excluded Provider: 21T101 -> 21 T 101
 #' @rdname parse
 #' @export
-parse_eipps_provider <- parser(
-  entity = "IPPS-Excluded Provider",
+parse_unit <- parser(
+  entity = "IPPS-Excluded Unit",
   regex  = "([MR-UWYZ])([0-9]{3})",
   groups = "\\2 \\3")
 
 # IPPS-Excluded Unit: 02TA01 -> 02 T A 01
 #' @rdname parse
 #' @export
-parse_eipps_unit <- parser(
-  entity = "IPPS-Excluded Unit",
+parse_subunit <- parser(
+  entity = "IPPS-Excluded Subunit",
   regex  = "([MR-UWYZ])([A-HJK])([0-9]{2})",
   groups = "\\2 \\3 \\4",
   names  = c("type", "parent", "sequence"))
