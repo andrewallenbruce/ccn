c7e16bfe472d22455d397e1b3ac4286c1ccd829c
Andrew Bruce <andrewbruce.himni@gmail.com>
2025-12-07 19:12:48 -0500

    * `ccn_` sketch of S7 initial parser * added `air` as formatter

--- R/is_type.R
+++ R/is_type.R
@@ -61,11 +61,14 @@ is_sub_type <- function(x) {
 #' @noRd
 ccn_type <- function(x) {
   kit::nif(
-    nchar_provider(x) | nchar_provider_ext(x), "provider",
-    nchar_supplier(x) | nchar_supplier_ext(x), "supplier",
+    nchar_provider(x), "provider",
+    nchar_provider_ext(x), "provider_ext",
+    nchar_supplier(x), "supplier",
+    nchar_supplier_ext(x), "supplier_ext",
     default = NA_character_
   )
 }
+
 
 #' @noRd
 provider_type <- function(x) {
@@ -79,3 +82,44 @@ provider_type <- function(x) {
     default = NA_character_
   )
 }
+
+#' @noRd
+substr_seq <- function(x) {
+  switch(provider_type(x),
+    organ     = ,
+    medicaid  = ,
+    unit      = substr(x, 4L, 6L),
+    medicare  = substr(x, 3L, 6L),
+    emergency = substr(x, 3L, 5L),
+    subunit   = substr(x, 5L, 6L),
+    NA_character_
+  )
+}
+
+#' @noRd
+substr_typ <- function(x) {
+  switch(
+    provider_type(x),
+    organ     = ,
+    medicaid  = ,
+    unit      = ,
+    subunit   = substr_(x, 3L),
+    emergency = substr_(x, 6L),
+    NA_character_
+  )
+}
+
+#' @noRd
+ccn_ <- S7::new_class(
+  name       = "ccn_",
+  properties = list(
+    num = S7::class_character,
+    ste = S7::new_property(getter = \(self) substr(self@num, 1L, 2L)),
+    seq = S7::new_property(getter = \(self) substr_seq(self@num)),
+    typ = S7::new_property(getter = \(self) substr_typ(self@num)),
+    par = S7::new_property(getter = \(self) {
+      if (provider_type(self@num) == "subunit") substr_(self@num, 4L) else NA_character_
+    }),
+    ext = S7::new_property(default = NA_character_)
+  )
+)
