7c0d154f2f7396de4906c4a24226ca53456fd847
Andrew Bruce <andrewbruce.himni@gmail.com>
2026-04-05 21:48:24 -0400

    * work on parser

--- data-raw/vctrs_rcrd.R
+++ data-raw/vctrs_rcrd.R
@@ -1,9 +1,92 @@
-# Medicare
+x <- "670055"
+x <- rep(x, 10)
+m <- purrr::map(x, SS_NNNN)
 
+x <- "67005F"
+x <- rep(x, 10)
+e <- purrr::map(x, SS_NNNT)
+
+purrr::list_transpose(c(m, e))
+
+NAMES_CCN <- c("form", "state", "sequence", "type", "parent", "ext")
+
+# Provider: 670055 => 67 0055
+SS_NNNN <- function(x) {
+  vctrs::vec_c(
+    "M",
+    substring(
+      x,
+      vctrs::vec_c(1L, 3L),
+      vctrs::vec_c(2L, 6L)
+    ),
+    vctrs::vec_rep(NA_character_, 3L)
+  ) |>
+    rlang::set_names(NAMES_CCN)
+}
+
+# Emergency: 12345E => 12 345 E
+SS_NNNT <- function(x) {
+  vctrs::vec_c(
+    "E",
+    substring(
+      x,
+      vctrs::vec_c(1L, 3L, 6L),
+      vctrs::vec_c(2L, 5L, 6L)
+    ),
+    vctrs::vec_rep(NA_character_, 2L)
+  ) |>
+    name_ccn(NAMES_CCN)
+}
+
+# Organ: 05P001 => 05 P 001
+# Medicaid: 01L008 => 01 L 008
+# Unit: 21T101 => 21 T 101
+SS_TNNN <- function(x, form) {
+  vctrs::vec_c(
+    match.arg(form, c("O", "A", "U")),
+    substring(
+      x,
+      vctrs::vec_c(1L, 4L, 3L),
+      vctrs::vec_c(2L, 6L, 3L)
+    ),
+    vctrs::vec_rep(NA_character_, 2L)
+  ) |>
+    name_ccn(NAMES_CCN)
+}
+
+# Subunit: 02TA01 => 02 T A 01
+SS_TPNN <- function(x) {
+  vctrs::vec_c(
+    "S",
+    substring(
+      x,
+      vctrs::vec_c(1L, 5L, 3L, 4L),
+      vctrs::vec_c(2L, 6L, 3L, 4L)
+    ),
+    vctrs::vec_rep(NA_character_, 1L)
+  ) |>
+    name_ccn(NAMES_CCN)
+}
+
+# Supplier: 10C0001062 => 10 C 0001062
+SS_TN10 <- function(x) {
+  vctrs::vec_c(
+    "P",
+    substring(
+      x,
+      vctrs::vec_c(1L, 6L, 3L),
+      vctrs::vec_c(2L, 10L, 5L)
+    ),
+    vctrs::vec_rep(NA_character_, 2L)
+  ) |>
+    name_ccn(NAMES_CCN)
+}
+
+# Medicare
 
 SS_NNNN <- \(x) substring(x, c(1L, 3L), c(2L, 6L))
 SS_NNNT <- \(x) substring(x, c(1L, 3L, 6L), c(2L, 5L, 6L))
-SS_TNNN <- \(x) substring(x, c(1L, 3L, 4L),  c(2L, 3L, 6L))
+SS_TNNN <- \(x) substring(x, c(1L, 3L, 4L), c(2L, 3L, 6L))
 SS_TPNN <- \(x) substring(x, c(1L, 3L, 4L, 5L), c(2L, 3L, 4L, 6L))
 
 purrr::map(c("670055", "670056"), form1) |>
@@ -14,7 +97,8 @@ purrr::map(c("670055", "670056"), form1) |>
 c("670055", "670056") |>
   substring(
     c(1L, 3L),
-    c(2L, 6L))
+    c(2L, 6L)
+  )
 # SS|NNNN
 
 # Emergency: 12345E => 12 345 E
@@ -57,13 +141,13 @@ c("670055", "670056") |>
   )
 # SS|T|NNNNNNN
 
-
 new_ccn_rcrd <- function(
-    state = character(),
-    type = character(),
-    parent = character(),
-    number = character(),
-    extension = character()) {
+  state = character(),
+  type = character(),
+  parent = character(),
+  number = character(),
+  extension = character()
+) {
   new_rcrd(
     list(
       S = state,
@@ -71,8 +155,9 @@ new_ccn_rcrd <- function(
       P = parent,
       N = number,
       X = extension
-      ),
-    class = "ccn_rcrd")
+    ),
+    class = "ccn_rcrd"
+  )
 }
 
 format.ccn_rcrd <- function(x, ...) {
@@ -89,7 +174,7 @@ format.ccn_rcrd <- function(x, ...) {
     vec_if_else(vec_detect_missing(parent), "", paste0(parent, "/")),
     number,
     vec_if_else(vec_detect_missing(ext), "", paste0("+", ext))
-    )
+  )
 
   out[is.na(state) | is.na(number)] <- NA
 
